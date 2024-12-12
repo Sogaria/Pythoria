7a9aeb930fe1503f7814e4d42da5c7aae19aa62b
Sogaria <scarevenage@gmail.com>
2024-12-12 15:52:05 +0100

    spirograph

--- Week 3/Day 18/main.py
+++ Week 3/Day 18/main.py
@@ -3,6 +3,8 @@ import random
 
 bob = Turtle()
 screen = Screen()
+screen.colormode(255)
+bob.speed(10)
 
 def is_close_to (pos1, pos2, tolerance):
     return abs(pos1[0] - pos2[0]) < tolerance and abs(pos1[1] - pos2[1]) < tolerance
@@ -25,19 +27,40 @@ def dashed_line(length: int):
 def drawing_art():
     for x in range(3, 11):
         angle = 360 / x
-        bob.color(random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1))
+        bob.color(random_color())
         for i in range(0, x):
             bob.forward(100)
             bob.right(angle)
 
+def random_color() -> tuple:
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+
 def random_walk():
     list_angles = [0, 90, 180, 270]
     while abs(bob.pos()[0]) < 1920/2 and abs(bob.pos()[1]) < 1080/2:
-        bob.color(random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1))
+        bob.pensize(random.randint(1, 10))
+        bob.color(random_color())
         bob.right(random.choice(list_angles))
         bob.forward(50)
         print(bob.pos())
 
-random_walk()
+def draw_circle(size: int):
+    angle = 360 / 50
+    for x in range(0, 50):
+        bob.forward(size)
+        bob.left(angle)
+
+def spirograph():
+    print(f"Drawing spirograph with a speed of {bob.speed()}")
+    angle = 360 / 50
+    for x in range(0, 50):
+        bob.color(random_color())
+        bob.circle(100)
+        bob.left(angle)
+
+spirograph()
 
 screen.mainloop()

2d87b0b706311a2570f655e1c02083f91f599b06
Sogaria <scarevenage@gmail.com>
2024-12-12 15:03:02 +0100

    turtle part 1

---
+++ Week 3/Day 18/main.py
@@ -0,0 +1,35 @@
+from turtle import Turtle, Screen
+import random
+
+bob = Turtle()
+screen = Screen()
+
+def is_close_to (pos1, pos2, tolerance):
+    return abs(pos1[0] - pos2[0]) < tolerance and abs(pos1[1] - pos2[1]) < tolerance
+
+def turtle_square():
+    bob.forward(100)
+    while not is_close_to(bob.pos(), (0, 0), 15):
+        bob.right(90)
+        bob.forward(100)
+        
+def dashed_line(length: int):
+    for x in range(0, length):
+        if x % 2 == 0:
+            bob.pendown()
+            bob.forward(5)
+        if x % 2 != 0:
+            bob.penup()
+            bob.forward(5)
+
+def drawing_art():
+    for x in range(3, 11):
+        angle = 360 / x
+        bob.color(random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1))
+        for i in range(0, x):
+            bob.forward(100)
+            bob.right(angle)
+
+drawing_art()
+
+screen.mainloop()

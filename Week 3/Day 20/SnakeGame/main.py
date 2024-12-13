158c7e0860a1605b3dbdcef8c72e69c08c704770
Sogaria <scarevenage@gmail.com>
2024-12-13 14:39:15 +0100

    snake improved movement

--- Week 3/Day 20/SnakeGame/main.py
+++ Week 3/Day 20/SnakeGame/main.py
@@ -12,6 +12,7 @@ def create_snake():
     for i in range(0, 3):
         snake_parts.append(Turtle("square"))
         snake_parts[i].penup()
+        snake_parts[i].setheading(0)
         snake_parts[i].color("white")
         x = -20 + i * 20
         snake_parts[i].goto(x, 0)
@@ -20,7 +21,6 @@ def move_snake():
     for i in range(len(snake_parts)-1, 0, -1):    
         pos = snake_parts[i-1].pos()        
         snake_parts[i].teleport(pos[0], pos[1])
-        print(f"Snake_part{i} teleported to pos of {i-1}")
     snake_parts[0].forward(20)
     pos = snake_parts[0].pos()
     if abs(pos[0]) >= 300:
@@ -28,11 +28,19 @@ def move_snake():
     elif abs(pos[1]) >= 300:
         snake_parts[0].teleport(pos[0], pos[1] * -1)
 
+def turn_left():
+    heading = snake_parts[0].heading()
+    snake_parts[0].setheading(heading+90)
+
+def turn_right():
+    heading = snake_parts[0].heading()
+    snake_parts[0].setheading(heading-90)
+
 gameOn = True
 create_snake()
 while gameOn == True:
-    window.update()
     move_snake()
-    time.sleep(0.1)
+    window.onkeyrelease(lambda: turn_left(), "a")
+    window.onkeyrelease(lambda: turn_right(), "d")
 
 window.mainloop()

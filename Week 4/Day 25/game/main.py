29d4a22d597c8b31a023f612cfa8b9bc054691a4
Sogaria <scarevenage@gmail.com>
2024-12-16 20:04:10 +0100

    missed states written to csv functionality

--- Week 4/Day 25/game/main.py
+++ Week 4/Day 25/game/main.py
@@ -17,16 +17,32 @@ print(state_data)
 def turtle_write(x: int, y: int, state: str):
     turtle.teleport(x, y)
     turtle.write(state, align="center")
-
+    
+def check_missed_states(list_guessed_states):
+    list_all_states = state_data.state.to_list()
+    missed_states = []
+    
+    for state in list_all_states:
+        if list_guessed_states.count(state) == 0:
+            missed_states.append(state)
+    new_Frame = pd.DataFrame(missed_states)
+    new_Frame.to_csv("Week 4/Day 25/game/missed_states.csv")
 #game
 correct_guess_states = []
 while len(correct_guess_states) < 50:
     prompt = window.textinput(f"{len(correct_guess_states)}/50 States Correct", "What's another state name?")
     prompt = prompt.title()
     
+    if prompt == "Exit":
+        check_missed_states(correct_guess_states)
+        break
     if state_data.state.to_list().count(prompt) > 0 and correct_guess_states.count(prompt) == 0:
         correct_guess_states.append(prompt)
         row = state_data[state_data["state"] == prompt]
         state, x, y = row[["state", "x", "y"]].values[0]
         turtle_write(x, y, state)
+
+
+
+
 window.mainloop()

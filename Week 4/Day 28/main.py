79041d876b94267021ba16680fa23169390fcaa8
Sogaria <scarevenage@gmail.com>
2024-12-19 16:05:37 +0100

    pomodoro with breaks

--- Week 4/Day 28/main.py
+++ Week 4/Day 28/main.py
@@ -12,45 +12,60 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER Variables ------------------------------- # 
 after_id = None
 timer_running = False
+minutes = 25
+seconds = 0
+sets = 0
+set_done = False
 # ---------------------------- TIMER RESET ------------------------------- # 
 def timer_reset():
     global timer_running
     if timer_running:
         window.after_cancel(after_id)
-        canvas.itemconfig(canvas_id, text="00:00")
+        canvas.itemconfig(canvas_id, text="25:00")
+        minutes = 25
+        seconds = 0
         timer_running = False
 # ---------------------------- TIMER START ------------------------------- # 
 def timer_start():
     global timer_running
     if not timer_running:
         timer_running = True
-        start_time = time.time()
-        update_timer(start_time)
+        update_timer(minutes, seconds)
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-def update_timer(start_time):
-    global after_id
-    elapsed_time = round(time.time() - start_time, 2)
-    minutes = int(elapsed_time // 60)
-    seconds = int(elapsed_time % 60)
+def update_timer(minutes, seconds):
+    global sets, after_id
+    if seconds >= 0:
+        seconds -= 1
+    if seconds == -1:
+        seconds = 59
+        minutes -= 1
     time_formatted = f"{minutes:02}:{seconds:02}"
     canvas.itemconfig(canvas_id, text=time_formatted)
-    
-    after_id = window.after(1000, update_timer, start_time)
+    if minutes == 0 and seconds == 0 and not set_done:
+        sets += 1
+        set_done = True
+        update_timer(5, 0)
+    if minutes == 0 and seconds == 0 and set_done:
+        set_done = False
+        update_timer(25, 0)
+    if sets == 4:
+        timer_reset()
+    after_id = window.after(1000, update_timer, minutes, seconds)
 # ---------------------------- UI SETUP ------------------------------- #
 window = tk.Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50, bg=YELLOW)
+window.config(padx=100, pady=50, bg=RED)
 
-label_title = tk.Label(text="Timer", fg=GREEN, font=(FONT_NAME, 40, "bold"), bg=YELLOW)
+label_title = tk.Label(text="Timer", fg=GREEN, font=(FONT_NAME, 40, "bold"), bg=RED)
 label_title.grid(row=0, column=1)
 
 button_start = tk.Button(text="Start", command=lambda: timer_start(), highlightthickness=0)
 button_start.grid(row=2, column=0)
 
-button_reset = tk.Button(text="Stop", command=lambda: timer_reset(), highlightthickness=0)
+button_reset = tk.Button(text="Reset", command=lambda: timer_reset(), highlightthickness=0)
 button_reset.grid(row=2, column=2)
 
-canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+canvas = tk.Canvas(width=200, height=224, bg=RED, highlightthickness=0)
 background = tk.PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=background)
 canvas_id = canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 30, "bold"), fill="white")

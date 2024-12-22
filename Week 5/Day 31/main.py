079d501f1eb4e71682630a88c97c3e0dccd543e5
Sogaria <scarevenage@gmail.com>
2024-12-22 15:09:38 +0100

    Card flipper done, button cd implemented aswell

--- Week 5/Day 31/main.py
+++ Week 5/Day 31/main.py
@@ -1,22 +1,19 @@
 import tkinter as tk
-import pandas, time
+import pandas, time, random
 
 BACKGROUND_COLOR = "#B1DDC6"
 words_csv = pandas.read_csv("data/french_words.csv")
 i = 0
 
-
-
 window = tk.Tk()
 window.title("Study Languages")
 window.configure(background=BACKGROUND_COLOR, padx=40, pady=40)
 #-------------------------UI-------------------------#
-
 #canvas
 canvas = tk.Canvas(width=800, height=526, bg=BACKGROUND_COLOR, bd=0, highlightthickness=0)
 card_front = tk.PhotoImage(file="images/card_front.png")
 card_back = tk.PhotoImage(file="images/card_back.png")
-#remember to edit state between 'normal' and 'hidden'
+
 id_card_front = canvas.create_image(400, 263, image=card_front)
 id_card_back = canvas.create_image(400, 263, image=card_back, state='hidden') 
 #store obj id to itemconfig canvas obj
@@ -35,27 +32,35 @@ no_button.grid(row=1, column=0)
 #--------------------Handle Cards--------------------#
 def flip_card(know_word: bool):
     global i, words_csv
+    #disable buttons until new card
+    no_button.config(state='disabled')
+    yes_button.config(state='disabled')
+    #flip cards
     canvas.itemconfig(id_card_front, state='hidden')
     canvas.itemconfig(id_card_back, state='normal')
     canvas.itemconfig(id_language_text, text="French")
     canvas.itemconfig(id_word_text, text=words_csv["French"][i])
     if know_word: #yes button
-        words_csv.drop(index=i)
-        print(f"Dropped {words_csv.iloc[i]}")
-        print(words_csv)
+        words_csv = words_csv.drop(index=i)
 
     #update window manually before after() method timer
     window.update_idletasks()
     
-    window.after(5000, new_card())
+    window.after(5000, new_card)
 
 def new_card():
     global i, words_csv
-    i += 1
-    canvas.itemconfig(id_word_text, text=words_csv["English"][i])
-    canvas.itemconfig(id_language_text, text="English")
-    canvas.itemconfig(id_card_front, state='normal')
-    canvas.itemconfig(id_card_back, state='hidden')
+    #activate buttons again
+    no_button.config(state='active')
+    yes_button.config(state='active')
+    if len(words_csv) > 1 and i < len(words_csv) - 1:
+        i += 1
+        canvas.itemconfig(id_word_text, text=words_csv["English"][i])
+        canvas.itemconfig(id_language_text, text="English")
+        canvas.itemconfig(id_card_front, state='normal')
+        canvas.itemconfig(id_card_back, state='hidden')
+    elif i == len(words_csv) - 1:
+        i = -1
+        new_card()
 
 window.mainloop()
-

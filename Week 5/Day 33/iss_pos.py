fa8feb9c0bcca222bd1f929afe2e6a410293b572
Sogaria <scarevenage@gmail.com>
2024-12-26 18:27:00 +0100

    ISS locator

--- Week 5/Day 33/iss_pos.py
+++ Week 5/Day 33/iss_pos.py
@@ -1,11 +1,11 @@
-import requests, datetime as dt
+import requests, datetime as dt, smtplib, schedule, time
+from geopy.geocoders import Nominatim
 
 MY_LATITUDE = 49.319538
 MY_LONGITUTE = 7.334330
 
-time = dt.datetime.now()
-if time.hour <= 8 or time.hour >= 17:
-    check_iss(true)
+my_email = "te2t3k53@gmail.com"
+my_pw = "qcuf pbrg uvtk wycr"
 
 def check_iss(is_night: bool):
     if is_night:
@@ -13,10 +13,42 @@ def check_iss(is_night: bool):
         r.raise_for_status()
         data_iss = r.json()
 
-        lat_iss = data_iss["iss_position"]["latitude"]
-        long_iss = data_iss["iss_position"]["longitude"]
+        lat_iss = float(data_iss["iss_position"]["latitude"])
+        long_iss = float(data_iss["iss_position"]["longitude"])
 
-        if abs(MY_LATITUDE - lat_iss) <= 2 and abs(MY_LONGITUTE-long_iss) <= 2:
+        if abs(MY_LATITUDE - lat_iss) <= 15 and abs(MY_LONGITUTE-long_iss) <= 9:
+            print(f"ISS is close enough at ({lat_iss},{long_iss})")
+
+            geolocator = Nominatim(user_agent="geoapi")
+            latitude = lat_iss
+            longitude = long_iss
+            location = geolocator.reverse((latitude, longitude))
+            print(f"Currently at {location.address}")
             send_mail()
+        else:
+            print(f"ISS is not close enough, currently at ({lat_iss},{long_iss})")
+            geolocator = Nominatim(user_agent="geoapi")
+            latitude = lat_iss
+            longitude = long_iss
+            location = geolocator.reverse((latitude, longitude))
+            print(f"Address: {location.address}")
+    else:
+        print("Right now its daytime at your location!")
 
 def send_mail():
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_pw)
+        connection.sendmail(from_addr=my_email, to_addrs="hackermaus@gmail.com", msg="Subject: ISS\n\nLook up!")
+
+def check_time():
+    time = dt.datetime.now()
+    print(time)
+    if time.hour <= 8 or time.hour >= 17:
+        check_iss(True)
+
+schedule.every(60).seconds.do(check_time)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)

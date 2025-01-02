00968d05f0d2f4e866ec8c78b47bd7eff99cb2e2
Sogaria <scarevenage@gmail.com>
2025-01-02 17:07:36 +0100

    check if raining in next 12 hours api

--- Week 5/Day 35/main.py
+++ Week 5/Day 35/main.py
@@ -1,14 +1,15 @@
-import requests, json
+import requests, json, twilio
 
 api_key = "f26a29d7ed9d42b869059f1f2e7ad4cf"
-lati = 49.32
-longi = 7.33
+lati = 47.32
+longi = 8.33
 api_call = "https://api.openweathermap.org/data/2.5/forecast"
 
 parameters = {
     "lat" : lati,
     "lon" : longi,
-    "appid" : api_key
+    "appid" : api_key,
+    "cnt" : 4
 }
 
 r = requests.get(api_call, params=parameters)
@@ -18,3 +19,9 @@ data = r.json()
 
 with open("weather_forecast.json", "w") as file:
     json.dump(data, file)
+
+#check if its snowing in next 12 hours
+for item in data["list"]:
+    if item["weather"][0]["main"] == "Rain": 
+        print("You will need an umbrella!")
+        break
